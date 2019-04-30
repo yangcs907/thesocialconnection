d32838bbc3a1e19b73319344bd928588de619f68
yangcs907 <>
2019-04-30 01:29:19 -0400

    removed dev tools extension for production

--- client/src/Store/index.js
+++ client/src/Store/index.js
@@ -11,8 +11,9 @@ const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // allows redux devtools compatibility
+    applyMiddleware(...middleware)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // allows redux devtools compatibility
   )
 );
 
